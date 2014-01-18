1a19a62aaf37c6f5f8e18081a3c1de9b8985ab55
toshimi.masada@gmail.com <toshimi.masada@gmail.com>
2014-01-19 02:20:17 +0900

    create practice repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Practice::Application.config.secret_key_base = 'afbbc11b1a21720a55d9c8f50ae70daaa5b903b6400b4955a1bbb0cd2a252b4da315ac155f9e563199bfbffd7af6e11c0880ffb3163b703a6e22c4bc18e35b0d'
